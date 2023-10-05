cc39cdc4252236c24e1e2b75b2d0ef87d217f240
Avas <avas.bajracharya@renegadeinsurance.com>
2023-10-05 11:55:18 +0545

    feat: added styles to character profile page

--- src/pages/ProfilePage/index.tsx
+++ src/pages/ProfilePage/index.tsx
@@ -1,40 +1,63 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import useFetchCharacterById from "../../hooks/queries/useFetchCharacterById";
-import { Box, Flex, List, Text, Title } from "@mantine/core";
+import { Accordion, Box, Center, Flex, List, Text, Title } from "@mantine/core";
+import classes from "./styles.module.css";
 
 const ProfilePage = () => {
   const { cid } = useParams();
   const { character } = useFetchCharacterById(cid || "");
   const characterObj = character?.[0];
-  console.log({ character, characterObj });
+
   return (
     <Box>
-      <Title order={1}>Marvel Character</Title>
-      <Flex>
-        <Text>Name:</Text>
-        <Text>{characterObj?.name}</Text>
-      </Flex>
-      <Flex>
-        <Text>Description:</Text>
-        <Text>{characterObj?.description}</Text>
-      </Flex>
-      <Flex>
-        <Text>Comics Appeared:</Text>
-        <List>
-          {characterObj?.comics?.items?.map((comic: any) => (
-            <List.Item>{comic.name}</List.Item>
-          ))}
-        </List>
-      </Flex>
-      <Flex>
-        <Text>Series:</Text>
-        <List>
-          {characterObj?.series?.items?.map((series: any) => (
-            <List.Item>{series.name}</List.Item>
-          ))}
-        </List>
-      </Flex>
+      <Title
+        order={1}
+        style={{
+          textAlign: "center",
+          color: "crimson",
+        }}
+        mt="4rem"
+        mb="2rem"
+      >
+        {characterObj?.name}
+      </Title>
+      <Center>
+        <Accordion
+          style={{
+            width: "80%",
+          }}
+          classNames={{
+            control: classes.header,
+          }}
+          mt="2rem"
+        >
+          <Accordion.Item value="Description">
+            <Accordion.Control>Description</Accordion.Control>
+            <Accordion.Panel>{characterObj?.description}</Accordion.Panel>
+          </Accordion.Item>
+          <Accordion.Item value="Comics">
+            <Accordion.Control>Comics</Accordion.Control>
+            <Accordion.Panel>
+              <List>
+                {characterObj?.comics?.items?.map((comic: any) => (
+                  <List.Item>{comic.name}</List.Item>
+                ))}
+              </List>
+            </Accordion.Panel>
+          </Accordion.Item>
+          <Accordion.Item value="Series">
+            <Accordion.Control>Series</Accordion.Control>
+            <Accordion.Panel>
+              <List>
+                {characterObj?.comics?.items?.map((comic: any) => (
+                  <List.Item>{comic.name}</List.Item>
+                ))}
+              </List>
+            </Accordion.Panel>
+          </Accordion.Item>
+        </Accordion>
+      </Center>
     </Box>
   );
 };
